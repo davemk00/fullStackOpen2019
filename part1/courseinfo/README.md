aaba986ed391d9458155f898d04e640bd9edef01
David K <david.king@zoho.com>
2019-10-30 21:45:50 +1100

    Part 1.3 - Course Info step3 update readme

--- part1/courseinfo/README.md
+++ part1/courseinfo/README.md
@@ -6,3 +6,6 @@ Separates the Header, Content and Total components from the App.
 I put the \<p\> tag into the components to make the App component clean.
 I put the "Number of exercises" text in the Total component too.
 
+**Second Commit** - step 2 - refactor to include \<Part ...
+
+**Third Commit** - step 3 - changes to variables
--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -43,21 +43,27 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
   
   
   return (
     <div>
       <Header str = {course} />
-	    <Content  str1 = {part1} num1 = {exercises1}
-                str2 = {part2} num2 = {exercises2}
-                str3 = {part3} num3 = {exercises3} />
-      <Total num = {exercises1 + exercises2 + exercises3} />
+	    <Content  str1 = {part1.name} num1 = {part1.exercises}
+                str2 = {part2.name} num2 = {part2.exercises}
+                str3 = {part3.name} num3 = {part3.exercises} />
+      <Total num = {part1.exercises + part2.exercises + part3.exercises} />
     </div>
   )
 }
