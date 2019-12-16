ee930ea25693d340aaea6b30694144da9f50d999
David K <david.king@zoho.com>
2019-12-16 21:47:22 +1100

    Part 2.8 phonebook Step3

--- part2/phonebook/README.md
+++ part2/phonebook/README.md
@@ -4,3 +4,5 @@
 **Commit 1** - Part 2.6 The Phonebook Step1
 
 **Commit 2** - Part 2.7 The Phonebook Step2
+
+**Commit 3** - Part 2.8 The Phonebook Step3
--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,28 +2,38 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567' }
   ])
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+
 
   const rows = () => persons.map(person => 
    // <p key={person.name} />
-    <p key={person.name}>{person.name}</p>
+    <p key={person.name}>{person.name} {person.number}</p>
   )
+
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
+
   const addPerson = (event) => {
     event.preventDefault()
-    const person = { name: newName }
+    const person = { name: newName, number: newNumber }
     const checkExists = persons.findIndex(person => person.name.toLowerCase() === newName.toLowerCase())
     checkExists > -1
-      ? window.alert(`${person.name} is already in the phone book`)
+      ? window.alert(person.name + ' is already in the phone book')
       : setPersons(persons.concat(person))
     setNewName('')
+    setNewNumber('')
   }
 
   return (    
@@ -37,6 +47,13 @@ const App = () => {
             onChange={handleNameChange}
           />
         </div>
+        <div>number: <input 
+            value = {newNumber}
+            onChange={handleNumberChange}
+          />
+        </div>
+
+
         <div>
           <button type="submit">add</button>
         </div>
